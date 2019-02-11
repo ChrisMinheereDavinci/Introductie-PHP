a92bbb1c73aa230024983493b3b2b4e77dec77d1
Chris_Minheere <99040522@mydavinci.nl>
2019-02-11 14:09:45 +0100

    PHP file added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="resources/css/main.css" />
+</head>
+
+<body>
+    <?php
+    echo "<h1> Hello World! </h1>";
+    echo "PHP  so easy!";
+  ?>
+</body>
+
+</html>
