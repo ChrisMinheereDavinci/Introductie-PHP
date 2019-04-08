2dc131699cc3d32217333837a978498ffd6a63cf
Chris_Minheere <99040522@mydavinci.nl>
2019-04-08 09:56:55 +0200

    Goede morgen/middag/avond/nacht added

---
+++ Goede_morgen_middag_avond_nacht/index.php
@@ -0,0 +1,49 @@
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <meta http-equiv="refresh" content="60">
+     <link rel="stylesheet" type="text/css" href="style.css">
+     <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+     
+   </head>
+   <body>
+
+   <?php
+   
+date_default_timezone_set("Europe/Amsterdam");
+$time = date("H:i:s");
+$morning = "Good morning! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
+$evening = "Good evening! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
+$afternoon = "Good afternoon! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
+$night = "Good night! ". date("H:i:s "). "<br>"." Today its " .date("d/m/Y");
+
+if ($time >= 6 && $time <12) {
+  echo "<h1>$morning</h1>";
+  echo '<body class = "morning">';
+  $day = $morning;
+}
+elseif ($time >= 12 && $time < 18) {
+  echo "<h1>$afternoon</h1>";
+  echo '<body class = "evening">';
+  $day = $afternoon;
+}
+
+elseif ($time >= 18 && $time < 00) {
+  echo "<h1>$evening</h1>";
+  echo '<body class = "afternoon">';
+  $day = $evening;
+}
+
+elseif ($time >= 00 && $time < 6) {
+  echo "<h1>$night</h1>";
+  echo '<body class = "night">';
+  $day = $night;
+}
+
+// echo "Have a ".$day." Chris"
+
+
+ ?>
+   </body>
+ </html>
