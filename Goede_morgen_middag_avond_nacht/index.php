0b89ac760ce534defa34bb2f0d4fcc5bedac1dc9
Chris_Minheere <99040522@mydavinci.nl>
2019-05-08 11:09:15 +0200

    time bugg fixt

--- Goede_morgen_middag_avond_nacht/index.php
+++ Goede_morgen_middag_avond_nacht/index.php
@@ -12,11 +12,11 @@
    <?php
    
 date_default_timezone_set("Europe/Amsterdam");
-$time = date("H:i:s");
-$morning = "Good morning! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
-$evening = "Good evening! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
-$afternoon = "Good afternoon! ". date("H:i:s ")."<br>". " Today its " .date("d/m/Y");
-$night = "Good night! ". date("H:i:s "). "<br>"." Today its " .date("d/m/Y");
+$time = '20:01:03';
+$morning = "Good morning! ". date("H:i ")."<br>". " Today its " .date("d/m/Y");
+$evening = "Good evening! ". date("H:i ")."<br>". " Today its " .date("d/m/Y");
+$afternoon = "Good afternoon! ". date("H:i ")."<br>". " Today its " .date("d/m/Y");
+$night = "Good night! ". date("H:i "). "<br>"." Today its " .date("d/m/Y");
 
 if ($time >= 6 && $time <12) {
   echo "<h1>$morning</h1>";
@@ -29,13 +29,13 @@ elseif ($time >= 12 && $time < 18) {
   $day = $afternoon;
 }
 
-elseif ($time >= 18 && $time < 00) {
+elseif ($time >= 18 && $time < 24) {
   echo "<h1>$evening</h1>";
   echo '<body class = "afternoon">';
   $day = $evening;
 }
 
-elseif ($time >= 00 && $time < 6) {
+elseif ($time >= 24 && $time < 6) {
   echo "<h1>$night</h1>";
   echo '<body class = "night">';
   $day = $night;
